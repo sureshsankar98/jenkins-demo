d6930d5b5b5f7bb71636b19ae427955014a1bf90
sureshsystem <myglobal@gmail.com>
2024-12-14 20:44:18 +0530

    my first commit suresh

---
+++ spring-boot-jenkins/src/main/java/com/examples/jenkins/SpringBootJenkinsApplication.java
@@ -0,0 +1,14 @@
+package com.examples.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJenkinsApplication.class, args);
+		System.out.println("Welcome to Jenkins Pipeline Demo...");
+	}
+
+}
